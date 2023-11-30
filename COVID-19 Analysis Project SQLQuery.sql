fb91609c7862e398da03ff76bfa2627753da1d56
Evelyn Zhao <alphaocean2016@gmail.com>
2023-11-29 22:47:52 -0500

    Update and rename COVID 19 Analysis Project SQLQuery.sql to COVID-19 Analysis Project SQLQuery.sql

--- COVID-19 Analysis Project SQLQuery.sql
+++ COVID-19 Analysis Project SQLQuery.sql
@@ -2,10 +2,10 @@
   --1. Introduction
 
     --The Covid pandemic has been affecting the world since late 2019, 
-    --causing unprecendented impacts on the health and economy of different countries and regions.
-    --This project aims to analyze the data and provide insights related to the COVID-19 testing, 
-    --vaccination, and mortality across various locations and time periods.
-	--It will especially examine how much damage covid 19 has caused to the world and 
+    --causing unprecedented impacts on the health and economy of different countries and regions.
+    --This project aims to analyze the data and provide insights related to COVID-19 testing, 
+    --vaccination, and mortality across various locations and periods.
+	--It will especially examine how much damage COVID-19 has caused to the world and 
 	--if the vaccinations and boosters were effective.
 
   --2. Data Source
@@ -13,9 +13,9 @@
     --The data used in this project comes from Our World in Data Coronavirus (COVID-19) Vaccinations
 	--and Coronavirus (COVID-19) Deaths datasets. 
 	--The Coronavirus (COVID-19) Vaccinations dataset contains information about the COVID-19 testing 
-	--and vaccination activities, as well as some demographic and socio-economic indicators, for each location and date.
+	--and vaccination activities and some demographic and socio-economic indicators for each location and date.
 	--The Coronavirus (COVID-19) Deaths dataset contains information about the COVID-19 cases and deaths, 
-	--as well as some health and hospitalization indicators, for each location and date.
+	--as well as some health and hospitalization indicators for each location and date.
   
   --3. Data Overview
 	SELECT *
@@ -23,7 +23,7 @@
 
 	SELECT *
 	FROM CovidDeaths;
-	--The datesets contain data updated until when I downloaded them. The numbers in the "total" columns
+	--The datasets contain data updated until when I downloaded them. The numbers in the "total" columns
 	--are aggregated by each date.
 
   --4. Data Exploration
@@ -58,12 +58,12 @@
 
 	EXEC (@sql_d);
 	--There are missing values in almost all the columns except for the iso_code, location, date, and population columns.
-	--This makes sense because at the beginning there are no cases or tests or vaccinations and countries has
-	--different ways and time of collecting information.
+	--This makes sense because at the beginning, there are no cases or tests or vaccinations, and countries have
+	--different ways and periods of collecting information.
 	
   --Data Cleaning
     
-	--The country names contain summarized rows which should not be included in further calculations.
+	--The country names contain summarized rows, which should not be included in further calculations.
 	--So it is better to get rid of these columns first.
 	SELECT location, COUNT(DISTINCT location) AS summarized_rows
 	FROM CovidVaccinations
@@ -93,14 +93,14 @@
 	FROM CovidVaccination
 	ORDER BY country_name;
 	
-	--Second, I want to measure the mortability and morbidity.
+	--Second, I want to measure the mortality and morbidity.
 	--What is the total number of cases, deaths, and death rate globally as of the latest date in the dataset?
 	SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases)*100 AS death_rate
     FROM CovidDeath;
-	--As of the latest date in the dataset, there are totally 772 million people infected by Covid 19, almost 7 million people died.
+	--As of the latest date in the dataset, there are a total of 772 million people infected by Covid 19, and almost 7 million people died.
 	--The global death rate is 0.9%.
 
-	--What is the total number of cases, deaths, and death rate for each continents as of the latest date in the dataset?
+	--What is the total number of cases, deaths, and death rate for each continent as of the latest date in the dataset?
 	SELECT continent, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases)*100 AS death_rate
     FROM CovidDeath
 	WHERE total_cases IS NOT NULL AND total_deaths IS NOT NULL
@@ -119,13 +119,13 @@
 	ORDER BY total_cases DESC;
 	ORDER BY total_deaths DESC;
 	ORDER BY death_rate DESC;
-	--As of the latest date in the dataset, the United States has the highest number of cases, about 103 million people affected.
+	--As of the latest date in the dataset, the United States has the highest number of cases, with about 103 million people affected.
 	--The countries rating second and third are China and India, with about 99 million and 45 million respectively.
-	--The United States still ranks number one over number of deaths, about 1 million people.
-	--Brazil and India rated second and third, with 70 thousands and 53 thousands respectively.
+	--The United States still ranks number one in number of deaths, about 1 million people.
+	--Brazil and India rated second and third, 70 thousand and 53 thousand respectively.
 	--Yemen has the highest death rate of about 18%, more than doubling the number of the second country, which is Sudan.
 	
-	--How many countries has above average death rate?
+	--How many countries have above-average death rates?
 	WITH cte AS (
 		SELECT location, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases)*100 AS death_rate
 		FROM CovidDeath
@@ -138,11 +138,11 @@
 		SELECT AVG(death_rate)
 		FROM cte
 		);
-	--Out of the 243 countries, almost one third of them, about 70 countries, have death rates higher than the global average.
+	--Out of the 243 countries, almost one-third of them, about 70 countries, have death rates higher than the global average.
 
 	--How does the excess mortality rate vary by month and year across different continents?
 	--The excess mortality rate is the percentage of deaths above the expected level based on the historical average. 
-	--It can be used as an indicator of the impact of the COVID 19 on the mortality. 
+	--It can be used as an indicator of the impact of COVID-19 on mortality. 
 	SELECT YEAR(date) AS [year], MONTH(date) AS [month], continent, AVG(CAST(excess_mortality AS float)) AS avg_exc_mort
 	FROM CovidVaccination
 	GROUP BY YEAR(date), MONTH(date), continent
@@ -150,7 +150,7 @@
 	--The highest excess mortality rates occur in South America, North America, and Africa, 
 	--whereas Asia, Europe, and Oceania have relatively lower rates.
 	--This may indicate that Asia and Europe have deaths spreading out over the three years 
-	--while the number of deaths occur in the other three continents may rise suddenly during a certain time period.
+	--while the number of deaths that occur in the other three continents may rise suddenly during a certain period.
 
 	--Third, I want to measure the testing and vaccination activities.
 	--What is the total number of tests, vaccinations, and boosters done globally as of the latest date in the dataset?
@@ -159,7 +159,7 @@
 	--As of the latest date in the dataset, about 9.2 billion tests were conducted, about 3.5 billion vaccinations 
 	--and 827 million boosters were taken around the world.
 
-	--How many countries has above average vaccination rate?
+	--How many countries have above-average vaccination rates?
 	CREATE VIEW vaccinated AS 
 		SELECT location, MAX(people_fully_vaccinated) AS people_fully_vaccinated, MAX(date) AS latest_date
 		FROM CovidVaccination
@@ -175,7 +175,7 @@
 	FROM cte
 	WHERE vaccination_rate > (SELECT AVG(vaccination_rate) FROM cte)
 	ORDER BY vaccination_rate DESC;
-	--Until the latest date in the dataset, almost half of the countries has above average vaccination rate.
+	--Until the latest date in the dataset, almost half of the countries have above-average vaccination rates.
 		
 	--rolling vaccinations vs. total population, which shows how the vaccinations are conducted in each country.
 	SELECT d.continent, d.location, d.date, population, new_vaccinations,
@@ -187,7 +187,7 @@
 	ORDER BY 2, 3;
 
 	--Next, I want to identify factors that may influence COVID-19 outcomes.
-	--total cases vs. population for each country, which shows the percentage of population got Covid
+	--total cases vs. population for each country, which shows the percentage of the population got COVID-19
 	WITH cte_cases AS (
 		SELECT iso_code, MAX(total_cases) AS total_cases
 		FROM CovidDeath
@@ -208,13 +208,13 @@
 	ORDER BY infection_rate DESC;
 	--The countries with the highest infection rate are those with small populations.
 
-	--Which countries have more than 50% of their population fully vaccinated and how much of their population living in extreme poverty?
+	--Which countries have more than 50% of their population fully vaccinated, and how much of their population is living in extreme poverty?
 	SELECT location, MAX(people_fully_vaccinated_per_hundred) as max_fully_vaccinated, CAST(extreme_poverty AS float) as extreme_poverty
 	FROM CovidVaccination
 	WHERE people_fully_vaccinated_per_hundred > 50 AND extreme_poverty IS NOT NULL
 	GROUP BY location, extreme_poverty
 	ORDER BY max_fully_vaccinated DESC;
-	--Seems proverty does not directly related to vaccination.
+	--It seems poverty is not directly related to vaccination.
 
 	--the percentage of female smokers and male smokers among the locations that have the highest and lowest total deaths per million from COVID-19
 	CREATE VIEW death_per_million AS
@@ -242,12 +242,12 @@
 	FROM cte_rank
 	WHERE death_rank = 1 OR death_rank = (SELECT MAX(death_rank) FROM cte_rank)
 	ORDER BY death_rank;
-	--In the country with the highest total deaths per million, almost one third of women smoke and almost half of men smoke,
-	--whereas the lowest country has only 0.1% of women and 15% men smoking. The differences are very big.
-	--Seems smoking has a great impact on dying from COVID 19.
+	--In the country with the highest total deaths per million, almost one-third of women smoke, and almost half of men smoke,
+	--whereas the lowest country has only 0.1% of women and 15% of men smoking. The differences are very big.
+	-It seems smoking has a great impact on dying from COVID-19.
 
 	--Finally, I want to measure the effectiveness of vaccinations and boosters.
-	--What are the top 10 countries with the highest ratio of people fully vaccinated to people tested positive for COVID-19?
+	--What are the top 10 countries with the highest ratio of people fully vaccinated to people who tested positive for COVID-19?
 	CREATE VIEW ratio AS
 		SELECT location, date, people_fully_vaccinated/(total_tests*positive_rate) AS ratio
 		FROM CovidVaccination
@@ -268,10 +268,10 @@
 
   --6. Conclusion
 
-	--The COVID 19 has a very negative impact to the world, causing millions of deaths and billions of infections.
-	--Almost one third of the world's coutries has above average death rate.
+	--COVID-19 has had a very negative impact on the world, causing millions of deaths and billions of infections.
+	--Almost one-third of the world's countries have above-average death rates.
 	--Fortunately, countries were taking positive actions. 
-	--Until the latest date in the dataset, almost half of the countries has above average vaccination rate.
+	--Until the latest date in the dataset, almost half of the countries have above-average vaccination rates.
 	--More insights can be generated from the dataset. This project is a small portion and a demo for the insights.
 
 
